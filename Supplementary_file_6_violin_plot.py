9ef53c10eddf7f1fd4b0b4d8ff1aaf5209256012
Nisha Paudel <39861089+Nishapaudel@users.noreply.github.com>
2022-06-28 16:16:06 +0545

    Update Supplementary_file_6_violin_plot.py

--- Supplementary_file_6_violin_plot.py
+++ Supplementary_file_6_violin_plot.py
@@ -18,12 +18,12 @@ from scipy.stats import norm
  
 violin_data = sys.argv[1]
 
-#violin_data ='/home/nisha/Desktop/Dinucleotide_project/model_organisms/Homo_sapiens/Homo_sapiens_violin_arranged.txt'
+
 df = pd.read_csv(violin_data, sep = '\t')
 
 violin = hv.Violin(df, ('Dinucleotide', 'Dinucleotides'), ('ratio', 'Ratio')).redim.range(ratio=(0, 2))
-violin.opts(height=500, width=900, violin_fill_color=dim('Dinucleotides').str(), cmap='Set1', title= 'exon_violin_plot')
+violin.opts(height=500, width=900, violin_fill_color=dim('Dinucleotides').str(), cmap='Set1', title= 'violin_plot')
 
 hv.save(violin, 'violin_plot.png', fmt = 'png' )
-#hv.opts(violin, 'Promo_violin_plot.png', fmt = 'png' )
+hv.opts(violin, 'violin_plot.png', fmt = 'png' )
 
