befa5ec75f1794d3f32bcad20e7a90fe88bf4a38
Nisha Paudel <39861089+Nishapaudel@users.noreply.github.com>
2022-07-21 15:51:18 +0545

    Update Supplementary_file_6_violin_plot.py

--- Supplementary_file_6_violin_plot.py
+++ Supplementary_file_6_violin_plot.py
@@ -7,12 +7,9 @@ hv.extension('bokeh')
  
 violin_data = sys.argv[1] #The input for this file is output file form supplementary file 5 code.
 
-
 df = pd.read_csv(violin_data, sep = '\t')
 
 violin = hv.Violin(df, ('Dinucleotide', 'Dinucleotides'), ('ratio', 'Ratio')).redim.range(ratio=(0, 2))
-violin.opts(height=500, width=900, violin_fill_color=dim('Dinucleotides').str(), cmap='Set1', title= 'violin_plot')
 
-hv.save(violin, 'violin_plot.png', fmt = 'png' )
-hv.opts(violin, 'violin_plot.png', fmt = 'png' )
-
+violin = hv.Violin(df, ('Dinucleotide', 'Dinucleotides'), ('ratio', 'Ratio')).redim.range(ratio=(0, 2))
+violin.opts(height=500, width=900, violin_fill_color=hv.dim('Dinucleotides').str(), cmap='Set1', title= 'violin_plot')
