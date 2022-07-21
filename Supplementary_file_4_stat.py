4843188320d4c7107c7c4e2bdc71749cc13595e1
Nisha Paudel <39861089+Nishapaudel@users.noreply.github.com>
2022-07-21 18:14:35 +0545

    important_change_on_stat

--- Supplementary_file_4_stat.py
+++ Supplementary_file_4_stat.py
@@ -1,20 +1,14 @@
-import numpy
-from scipy import stats
 import pandas as pd
+#file = '/home/nisha/Desktop/Dinucleotide_project/all_vertebrates/Acanthisitta_chloris/Acanthisitta_chloris_exon.txt'
 import sys
-import os
 
-file= sys.argv[1] # The input of the file is the output ratio file from supplementary file 3. and output should look like 'Zerene_cesonia_exon_stat.txt'
+file= sys.argv[1]
+lyst = []
 df = pd.read_csv(file, sep = '\t')
 df
-print('Mean','Median','Mode' ,' std', 'var', 'skew', 'kurtosis', sep = '\t')
-for i in df.columns[4:20]:
-    x = df[i].mean()
+for i in df.columns[5:21]:
+    #x = df[i].mean()
     y = df[i].median()
-    z = df[i].mode()
-    a = df[i].std()
-    b = df[i].var()
-    c = df[i].skew()
-    d = df[i].kurtosis()
-    print(x, y ,z ,a, b, c, d, sep = '\t')
-    #print(z)
+    lyst.append(y) 
+print(lyst)
+#lyst.clear()
